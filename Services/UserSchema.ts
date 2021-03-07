90c9a40ae6a42a912cb3aa7ae0726d7eef0f52c5
richardwellerson <richardwellerson@gmail.com>
2021-03-07 09:28:50 -0300

    ORMConfig: Add validators at UserSchema

--- Services/UserSchema.ts
+++ Services/UserSchema.ts
@@ -2,8 +2,28 @@ import Joi from 'joi';
 
 const User: object = Joi.object({
   name: Joi.string()
-  .min(3)
-  .max(50)
-  .pattern(/[a-zçáéíóúàèìòùãâêîôû]/gi)
-  .required(),
+    .min(3)
+    .max(50)
+    .pattern(/[a-zçáéíóúàèìòùãâêîôû]/gi)
+    .required(),
+  email: Joi.string()
+    .min(6)
+    .max(70)
+    .email()
+    .required(),
+  phone: Joi.string()
+    .min(11)
+    .max(13)
+    .pattern(/[0-9]/)
+    .required(),
+  driverLicense: Joi.string()
+    .min(8)
+    .max(20)
+    .alphanum()
+    .required(),
+  vehicle: Joi.string()
+    .min(7)
+    .max(9)
+    .alphanum()
+    .required(),
 });
